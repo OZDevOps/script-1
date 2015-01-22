47f863b5ec4124bbc1a447762cfe29c88651f501
OZDevOps <OZDevOps@users.noreply.github.com>
2015-01-22 13:58:58 +1100

    Create connect.sh

---
+++ connect.sh
@@ -0,0 +1,53 @@
+vagrant@web01:~$ cat connect.sh
+#!/usr/bin/env bash
+
+# pre-request
+# 1. script tested on Ubuntu
+# 2. passwordless ssh has been set to all input ipaddress
+# 3. nc command is available.
+
+USAGE="Usage: $0 ip port1 port2 ... portN"
+
+if [ "$#" -lt "2" ]; then
+	echo "$USAGE"
+	exit 1
+fi
+
+ipaddress=$1
+shift
+
+while (( "$#" )); do
+
+  port=$1
+
+  if [ "$port" == "127.0.0.1" ]; then
+      SUDO="sudo"
+  else
+      SUDO="ssh $ipaddress sudo"
+  fi
+
+  nc "$ipaddress" "$port" < /dev/null
+  status="$?"
+
+  if [ "$status" == "0" ]; then
+     echo "port $port is runnnig well on $ipaddress"
+  else
+      case $port in
+          "80" | "443" ) echo "webservice is down, restarting ...."
+                         $SUDO service apache2 restart
+                         ;;
+          "22" ) echo "ssh service is down, restart ..."
+                 if [ "$port" == "127.0.0.1" ]; then
+                      sudo service ssh restart
+                 else
+                     echo "Can't help to start ssh instance on remote client $ipaddress"
+                 fi
+                 ;;
+           * )   echo "not sure which service running on port $port"
+                 ;;
+     esac
+  fi
+
+  shift
+
+done
